20c6e058b3cc88922868a273a84f26630df3416b
liyasthomas <liyascthomas@gmail.com>
2019-04-13 12:33:11 +0530

    :rotating_light: Lint

--- src/head.js
+++ src/head.js
@@ -4,17 +4,15 @@ function fetchJSON(a) {
 	var f = new XMLHttpRequest();
 	f.open("GET", a, false);
 	f.send(null);
-	return f.responseText
+	return f.responseText;
 }
 
 function isURL(a) {
 	var f = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 	if (f.test(a)) {
-		console.log(f.test(a));
-		return true
+		return true;
 	} else {
-		console.log(f.test(a));
-		return false
+		return false;
 	}
 }
 var hashh = window.location.hash.substr(1);
@@ -23,7 +21,7 @@ if (window.location.hash != "") {
 	var data = res.result;
 	if (data != null) {
 		if (isURL(data)) {
-			window.location.href = data
+			window.location.href = data;
 		}
 	}
 }
