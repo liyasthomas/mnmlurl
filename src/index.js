d87043f9aed130673ac9ad463d346bb7b9e4add6
liyasthomas <liyascthomas@gmail.com>
2019-06-22 10:08:32 +0530

    :sparkles: Added install PWA banner button

--- src/index.js
+++ src/index.js
@@ -157,3 +157,41 @@ sbtn.addEventListener('click', shorturl)
 // });
 // let r = JSON.parse(fetchJSON(endpoint)).result;
 // document.getElementById("count").innerHTML = Object.keys(r).length + " urls minimalized";
+let pwaInstalled = localStorage.getItem('pwaInstalled') == 'yes'
+if (window.matchMedia('(display-mode: standalone)').matches) {
+	localStorage.setItem('pwaInstalled', 'yes')
+	pwaInstalled = true
+}
+if (window.navigator.standalone === true) {
+	localStorage.setItem('pwaInstalled', 'yes')
+	pwaInstalled = true
+}
+if (pwaInstalled) {
+	document.getElementById('installPWA').style.display = 'none'
+} else {
+	document.getElementById('installPWA').style.display = 'inline-flex'
+}
+let deferredPrompt = null
+window.addEventListener('beforeinstallprompt', (e) => {
+	deferredPrompt = e
+})
+async function installPWA() {
+	if (deferredPrompt) {
+		deferredPrompt.prompt()
+		deferredPrompt.userChoice.then(({
+			outcome
+		}) => {
+			if (outcome === 'accepted') {
+				console.log('Your PWA has been installed')
+			} else {
+				console.log('User chose to not install your PWA')
+			}
+			deferredPrompt = null
+		})
+	}
+}
+window.addEventListener('appinstalled', (evt) => {
+	localStorage.setItem('pwaInstalled', 'yes')
+	pwaInstalled = true
+	document.getElementById('installPWA').style.display = 'none'
+})
