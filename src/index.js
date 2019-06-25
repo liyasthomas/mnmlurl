a546696bd06f7b53c238f224e802dba4faf621bd
liyasthomas <liyascthomas@gmail.com>
2019-06-25 23:41:53 +0530

    :bookmark: v2.0.0

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ let send_request = (url) => {
 	let address = `${endpoint}/${window.location.hash.substr(1)}`
 	// console.log(address)
 	pushJSON(address, myurl)
-	output.style.display = 'block'
+	output.style.display = 'flex'
 	shortenedURL.value = window.location.href
 	copyer('shortenedURL')
 	sucess.innerHTML = 'short url copied to clipboard'
