a21f2c814b446645744c8e0dd8e04c0188851375
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2019-12-08 22:31:32 +0300

    Add Url Protocol Checker

--- src/head.js
+++ src/head.js
@@ -22,16 +22,26 @@ const isURL = (a) => {
 	const f = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
 	if (f.test(a)) {
 		return true
-	} else {
-		return false
-	}
+	}	
+	return false
 }
+
+const startWithProtocol = (a) => {
+	if(/^(f|ht)tps?:\/\//i.test(a)) {
+            return true
+        }	
+	return false
+}
+
 const hashh = window.location.hash.substr(1)
 if (window.location.hash != '') {
 	const res = JSON.parse(fetchJSON(`${endpoint}/${hashh}`))
 	const data = res.result
 	if (data != null) {
 		if (isURL(data)) {
+			if(!startWithProtocol(a)) {
+				data = `http://${data}`;
+			}
 			window.location.href = data
 		}
 	}
